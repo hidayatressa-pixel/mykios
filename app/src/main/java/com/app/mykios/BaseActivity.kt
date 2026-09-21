f79c7eb270359805732d654c16632e628aee0649
Ressa Hidayat <hidayatressa@gmail.com>
2026-09-21 21:28:06 +0700

    Centralize theme startup and modernize settings permissions

--- app/src/main/java/com/app/mykios/BaseActivity.kt
+++ app/src/main/java/com/app/mykios/BaseActivity.kt
@@ -1,25 +1,11 @@
 package com.app.mykios
 
 import android.content.Context
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
 
 open class BaseActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        val session = SessionManager(this)
-        if (session.isDarkMode()) {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-        } else {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-        }
-        super.onCreate(savedInstanceState)
-    }
-
     override fun attachBaseContext(newBase: Context) {
         val session = SessionManager(newBase)
-        val lang = session.getLanguage()
-        super.attachBaseContext(LocaleHelper.setLocale(newBase, lang))
+        super.attachBaseContext(LocaleHelper.setLocale(newBase, session.getLanguage()))
     }
-
 }
